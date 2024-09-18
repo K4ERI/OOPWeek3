19de2f1020a874b83c7d60f4b3b0356c1a17259d
Khairin <Khairin@LAPTOP-IBAGE6OG>
2024-09-18 08:59:11 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        //creating an object (variable of certain class type)
+        Trransaction trransaction = new Trransaction();
+
+        trransaction.month = 1;
+        trransaction.val = 10.5f;
+
+        Console.WriteLine("transaction value is " + trransaction.val);
+    }
+}
